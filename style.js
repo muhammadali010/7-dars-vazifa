ca5c133f5b6cd70ad0d0652cb31e08389f1e4ba9
muhammadali010 <muhammadjonovmuhammadali32@gmail.com>
2024-05-31 14:57:05 +0500

    switch boldi

---
+++ style.js
@@ -0,0 +1,144 @@
+// 1chi masala
+// let n = +prompt("raqam kiriting");
+// switch (n) {
+//     case 1:
+//         console.log("yanvar");
+//         break;
+//     case 2:
+//         console.log("fevral");
+//         break;
+//     case 3:
+//         console.log("mart");
+//         break;
+//     case 4:
+//         console.log("aprel");
+//         break;
+//     case 5:
+//         console.log("may");
+//         break;
+//     case 6:
+//         console.log("iyun");
+//         break;
+//     case 7:
+//         console.log("iyul");
+//         break;
+//     case 8:
+//         console.log("avgust");
+//         break;
+//     case 9:
+//         console.log("sentyabr");
+//         break;
+//         case 10:
+//             console.log("oktyabr");
+//             break;
+//             case 11:
+//                 console.log("noyabr");
+//                 break;
+//                 case 12:
+//                     console.log("dekabr");
+//                     break;
+//     default:
+//         console.log("bunday oy yoq");
+//         break;
+// }
+
+
+// 2chi masala
+// let n = +prompt("baxoni kiriting");
+// switch (n) {
+//     case 91:
+//     case 92:
+//     case 93:
+//     case 94:
+//     case 95:
+//     case 96:
+//     case 97:
+//     case 98:
+//     case 99:
+//     case 100:
+//         console.log("A");
+//         break;
+//     case 81:
+//     case 82:
+//     case 83:
+//     case 84:
+//     case 85:
+//     case 86:
+//     case 87:
+//     case 88:
+//     case 89:
+//     case 90:
+//         console.log("B");
+//         break;
+//     case 71:
+//     case 72:
+//     case 73:
+//     case 74:
+//     case 75:
+//     case 76:
+//     case 77:
+//     case 78:
+//     case 79:
+//     case 80:
+//         console.log("C");
+//         break;
+//     case 60:
+//     case 61:
+//     case 62:
+//     case 63:
+//     case 64:
+//     case 65:
+//     case 66:
+//     case 67:
+//     case 68:
+//     case 69:
+//     case 70:
+//         console.log("D");
+//         break;
+//     default:
+//         console.log("F");
+//         break;
+// }
+
+
+// 3chi masala
+// let n = + prompt("soatni kiriting");
+// switch (n) {
+//     case 5:
+//     case 6:
+//     case 7:
+//     case 8:
+//     case 9:
+//     case 10:
+//     case 11:
+//     case 12:
+//         console.log("xayrli tong");
+//         break;
+//     case 13:
+//     case 14:
+//     case 15:
+//     case 16:
+//     case 17:
+//         console.log("xayrli kun");
+//         break;
+//     case 18:
+//     case 19:
+//     case 20:
+//     case 21:
+//         console.log("xayrli kech");
+//         break;
+//     case 22:
+//     case 23:
+//     case 24:
+//     case 0:
+//     case 1:
+//     case 2:
+//     case 3:
+//         case 4:
+//         console.log("xayrli tun");
+//         break;
+
+//     default:
+// console.log("jicha uhlavong");
+//         break;
+// }
